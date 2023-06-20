c1c144d397a3ba2390e5c53ae5bb6b485626fb89
CHia <dozeuto@gmail.com>
2023-06-20 16:00:05 +0800

    Create LinkedList.swift

---
+++ Sources/LinkedLists/LinkedList.swift
@@ -0,0 +1,139 @@
+//
+//  LinkedList.swift
+//  
+//
+//  Created by Chia on 2023/6/20.
+//
+
+import Foundation
+
+extension LinkedLists {
+    public struct LinkedList<Value> {
+        public var head: Node<Value>?
+        public var tail: Node<Value>?
+        
+        public init() {}
+        
+        public var isEmpty: Bool {
+            head == nil
+        }
+        
+        public mutating func push(_ value: Value) {
+            head = Node(value: value, next: head)
+            if tail == nil {
+                tail = head
+            }
+        }
+        
+        public mutating func append(_ value: Value) {
+            guard !isEmpty else {
+                push(value)
+                return
+            }
+            tail?.next = Node(value: value)
+            tail = tail?.next
+        }
+        
+        public func node(at index: Int) -> Node<Value>? {
+            var currentNode = head
+            var currentIndex = 0
+            
+            while currentNode != nil && currentIndex < index {
+                currentNode = currentNode?.next
+                currentIndex += 1
+            }
+            
+            return currentNode
+        }
+        
+        @discardableResult
+        public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+            guard tail !== node else {
+                append(value)
+                return tail!
+            }
+            
+            node.next = Node(value: value, next: node.next)
+            return node.next!
+        }
+        
+        public mutating func pop() -> Value? {
+            defer {
+                head = head?.next
+                if isEmpty {
+                    tail = nil
+                }
+            }
+            return head?.value
+        }
+        
+        @discardableResult
+        public mutating func removeLast() -> Value? {
+            guard let head else {
+                return nil
+            }
+            
+            guard head.next != nil else {
+                return pop()
+            }
+            
+            var prev = head
+            var current = head
+            
+            while let next = current.next {
+                prev = current
+                current = next
+            }
+            
+            prev.next = nil
+            tail = prev
+            return current.value
+        }
+        
+        @discardableResult
+        public mutating func remove(after node: Node<Value>) -> Value? {
+            defer {
+                if node.next === tail {
+                    tail = node
+                }
+                node.next = node.next?.next
+            }
+            return node.next?.value
+        }
+        
+        @discardableResult
+        public mutating func remove(at node: Node<Value>) -> Value? {
+            guard node !== head else {
+                return pop()
+            }
+            guard node !== tail else {
+                return removeLast()
+            }
+            
+            var prev = head
+            var current = head
+            
+            while let next = current?.next {
+                prev = current
+                current = next
+                if next === node {
+                    prev?.next = next.next
+                    break
+                }
+            }
+            
+            
+            
+            return current?.value
+        }
+    }
+}
+
+extension LinkedLists.LinkedList: CustomStringConvertible {
+    public var description: String {
+        guard let head else {
+            return "Empty list"
+        }
+        return String(describing: head)
+    }
+}
