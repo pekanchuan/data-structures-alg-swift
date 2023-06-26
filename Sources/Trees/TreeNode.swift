d87dad7e9a164c2ad7175081ddae523ca89eccfe
CHia <dozeuto@gmail.com>
2023-06-26 17:45:21 +0800

    Create TreeNode.swift

---
+++ Sources/Trees/TreeNode.swift
@@ -0,0 +1,75 @@
+//
+//  File.swift
+//  
+//
+//  Created by Chia on 2023/6/26.
+//
+
+import Foundation
+
+public class TreeNode<T> {
+    public var value: T
+    public var children: [TreeNode] = []
+    
+    public init(_ value: T) {
+        self.value = value
+    }
+    
+    public func add(_ child: TreeNode) {
+        children.append(child)
+    }
+    
+    
+}
+
+//  MARK: - Depth-first traversal
+
+extension TreeNode {
+    public func forEachDepthFirst(visit: (TreeNode) -> Void) {
+        visit(self)
+        children.forEach {
+            $0.forEachDepthFirst(visit: visit)
+        }
+    }
+}
+
+
+func makeBeverageTree() -> TreeNode<String> {
+    let tree = TreeNode("Beverages")
+    
+    let hot = TreeNode("hot")
+    let cold = TreeNode("cold")
+    
+    let tea = TreeNode("tea")
+    let coffee = TreeNode("coffee")
+    let chocolate = TreeNode("cocoa")
+    
+    let blackTea = TreeNode("black")
+    let greedTea = TreeNode("green")
+    let chaiTea = TreeNode("chai")
+    
+    let soda = TreeNode("soda")
+    let milk = TreeNode("milk")
+    
+    let gingerAle = TreeNode("ginger ale")
+    let bitterLemon = TreeNode("bitter lemon")
+    
+    tree.add(hot)
+    tree.add(cold)
+    
+    hot.add(tea)
+    hot.add(coffee)
+    hot.add(chocolate)
+    
+    cold.add(soda)
+    cold.add(milk)
+    
+    tea.add(blackTea)
+    tea.add(greedTea)
+    tea.add(chaiTea)
+    
+    soda.add(gingerAle)
+    soda.add(bitterLemon)
+    
+    return tree
+}
