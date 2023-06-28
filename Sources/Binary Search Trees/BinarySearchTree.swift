ed3dc37bac38c8f92aa1497c8ce4ee85bdf87b4c
CHia <dozeuto@gmail.com>
2023-06-28 13:18:52 +0800

    Update BinarySearchTree.swift

--- Sources/Binary Search Trees/BinarySearchTree.swift
+++ Sources/Binary Search Trees/BinarySearchTree.swift
@@ -61,3 +61,40 @@ extension BinarySearchTree {
         return false
     }
 }
+
+private extension BinaryNode {
+    var min: BinaryNode {
+        leftChild?.min ?? self
+    }
+}
+
+extension BinarySearchTree {
+    public mutating func remove(_ value: Element) {
+        root = remove(node: root, value: value)
+    }
+    
+    private func remove(node: BinaryNode<Element>?, value: Element) -> BinaryNode<Element>? {
+        guard let node else {
+            return nil
+        }
+        if value == node.value {
+            if node.leftChild == nil && node.rightChild == nil {
+                return nil
+            }
+            if node.leftChild == nil {
+                return node.rightChild
+            }
+            if node.rightChild == nil {
+                return node.leftChild
+            }
+            
+            node.value = node.rightChild!.min.value
+            node.rightChild = remove(node: node.rightChild, value: node.value)
+        } else if value < node.value {
+            node.leftChild = remove(node: node.leftChild, value: value)
+        } else {
+            node.rightChild = remove(node: node.rightChild, value: value)
+        }
+        return node
+    }
+}
