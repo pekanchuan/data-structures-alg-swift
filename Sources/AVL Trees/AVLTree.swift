6baf4669fd29079b56fa8aba0e544ad1a968aa67
CHia <dozeuto@gmail.com>
2023-06-29 17:06:45 +0800

    Update AVLTree.swift
    update remove(node, value) to balance

--- Sources/AVL Trees/AVLTree.swift
+++ Sources/AVL Trees/AVLTree.swift
@@ -137,11 +137,14 @@ extension AVLTree {
             node.value = node.rightChild!.min.value
             node.rightChild = remove(node: node.rightChild, value: node.value)
         } else if value < node.value {
-            node.leftChild = remove(node: node.leftChild, value: node.value)
+            node.leftChild = remove(node: node.leftChild, value: value)
         } else {
-            node.rightChild = remove(node: node.rightChild, value: node.value)
+            node.rightChild = remove(node: node.rightChild, value: value)
         }
 
-        return node
+        let balancedNode = balanced(node)
+        balancedNode.height = max(balancedNode.leftHeight,
+        balancedNode.rightHeight) + 1
+        return balancedNode
     }
 }
