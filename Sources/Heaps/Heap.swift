0727ece2a1f0032a72cd45080b2e5978499e5ae3
CHia <dozeuto@gmail.com>
2023-07-17 16:52:29 +0800

    Update Heap.swift

--- Sources/Heaps/Heap.swift
+++ Sources/Heaps/Heap.swift
@@ -30,4 +30,54 @@ struct Heap<Element: Equatable> {
     func parentIndex(ofChildAt index: Int) -> Int {
         (index - 1) / 2
     }
+
+    mutating func remove() -> Element? {
+        guard !isEmpty else {
+            return nil
+        }
+
+        elements.swapAt(0, count - 1)
+        defer {
+            siftDown(from: 0)
+        }
+
+        return elements.removeLast()
+    }
+
+    mutating func siftDown(from index: Int) {
+        var parent = index
+        while true {
+            let left = leftChildIndex(ofParentAt: parent)
+            let right = rightChildIndex(ofParentAt: parent)
+            var candidate = parent
+            if left < count && sort(elements[left], elements[candidate]) {
+                candidate = left
+            }
+
+            if right < count && sort(elements[right], elements[candidate]) {
+                candidate = right
+            }
+
+            if candidate == parent {
+                return
+            }
+            elements.swapAt(parent, candidate)
+            parent = candidate
+        }
+    }
+
+    mutating func insert(_ element: Element) {
+        elements.append(element)
+        siftUp(from: elements.count - 1)
+    }
+
+    mutating func siftUp(from index: Int) {
+        var child = index
+        var parent = parentIndex(ofChildAt: child)
+        while child > 0 && sort(elements[child], elements[parent]) {
+            elements.swapAt(child, parent)
+            child = parent
+            parent = parentIndex(ofChildAt: child)
+        }
+    }
 }
